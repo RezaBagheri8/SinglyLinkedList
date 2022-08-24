13e3a476be09b3a13f40bef8f181478572c26517
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-24 11:40:22 +0430

    Initial Repo

---
+++ SinglyLinkedList/SinglyLinkedList/LinkedList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Insert(int value)
+        {
+            if (head == null)
+            {
+                Node newItem = new Node();
+                newItem.value = value;
+                head = newItem;
+            }
+            else
+            {
+                Node newItem = new Node();
+                newItem.value = value;
+
+                Node node = head;
+                while (node.next != null)
+                {
+                    node = node.next;
+                }
+                node.next = newItem;
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The list is empty!");
+            }
+            else
+            {
+                Node node = head;
+                while (node != null)
+                {
+                    Console.WriteLine(node.value);
+                    node = node.next;
+                }
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SinglyLinkedList/SinglyLinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿using SinglyLinkedList;
+
+var list = new LinkedList();
+
+list.Insert(2);
+list.Insert(4);
+list.Insert(25);
+list.Insert(60);
+
+list.Print();
